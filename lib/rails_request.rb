05742006c540be98773135f95f1e1b3ebe4ab7bf
Bob Farrell <git@bob.frl>
2020-07-22 15:05:26 +0100

    Fix load ordering
    Ensure ApplicationControllerExtension class is loaded before we try to
    use it.

--- lib/rails_request.rb
+++ lib/rails_request.rb
@@ -5,8 +5,8 @@ require 'tempfile'
 require 'rails'
 
 require 'rails_request/version'
-require 'rails_request/railtie' if Rails.env.development?
 require 'rails_request/application_controller_extension'
+require 'rails_request/railtie' if Rails.env.development?
 require 'rails_request/payload'
 
 module RailsRequest
